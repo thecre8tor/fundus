5d44f2a6a9d142ca18137f7ae2554da4690d9429
Alexander Nitiola <anitiola@aellacredit.com>
2025-06-12 22:50:47 +0100

    project scafolled

---
+++ programs/fundus/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("BgBqnzoDwekLrMWCPJXcbx3V7naXVgqHZzXNjHi6Lgvb");
+
+#[program]
+pub mod fundus {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
