7e7fd72ac8eb58d82bab6ca360964865934ec294
Alexander Nitiola <cre8tor.alexander@gmail.com>
2025-07-21 13:56:29 +0100

    created all needed modules and files

--- programs/fundus/src/lib.rs
+++ programs/fundus/src/lib.rs
@@ -2,17 +2,19 @@
 #![allow(deprecated)]
 use anchor_lang::prelude::*;
 
+pub mod constants;
+pub mod errors;
+pub mod instructions;
+pub mod states;
+
+use instructions::*;
+use states::*;
+
 declare_id!("BgBqnzoDwekLrMWCPJXcbx3V7naXVgqHZzXNjHi6Lgvb");
 
 #[program]
 pub mod fundus {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
-}
 
-#[derive(Accounts)]
-pub struct Initialize {}
+}
